3e152c246c85dfc2ed4d02a64a654ab16cd2a8d5
Pablo Cocciaglia <cocciaglia@gmail.com>
2025-12-08 02:42:59 +0100

    remove extra tags in form-components

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
 import { FormTypes } from '../../interfaces/form.interface';
 import { EvaluationForm } from '../../components/evaluation-form/evaluation-form';
 import { TherapyForm } from '../../components/therapy-form/therapy-form';
@@ -11,8 +11,11 @@ import { TherapyForm } from '../../components/therapy-form/therapy-form';
 export class Home {
   formtypes: typeof FormTypes = FormTypes;
   form = signal<FormTypes | null>(null);
-
+  private readonly scrollElement = viewChild<ElementRef<HTMLDivElement>>('scrollHere');
   openEvaluationForm(formtypes: FormTypes) {
     this.form.set(formtypes);
+    setTimeout(() => {
+      this.scrollElement()?.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }, 20);
   }
 }
