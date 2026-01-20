cccd690eb7958fc43b3d5fe2c0ae279591c8c0b2
Pablo Cocciaglia <cocciaglia@gmail.com>
2026-01-20 18:44:02 +0100

    photo changed

--- src/app/pages/services/texts.ts
+++ src/app/pages/services/texts.ts
@@ -2,22 +2,22 @@ export const servicesTexts = [
   {
     title: 'Evaluaciones de Autismo',
     description:
-      'Valoración integral y actualizada que identifica fortalezas, desafíos y necesidades de apoyo.',
+      'Evaluación integral y actualizada de los aspectos conductuales, tanto retrospectivos como actuales, orientada a identificar desafíos y necesidades de apoyo. Se fundamenta en los criterios del DSM-5, incorporando una mirada actualizada, e incluye la recolección de información aportada por el entorno cercano del/la paciente (familia, pareja y amistades).',
   },
   {
     title: 'Evaluaciones de TDAH',
     description:
-      'Exploración clínica y funcional para comprender el perfil atencional y planificar intervenciones adecuadas.',
+      'Evaluación clínica, conductual y neurocognitiva orientada a comprender el perfil cognitivo del/la paciente y a planificar intervenciones acordes a sus necesidades. Dado que se trata de una condición del neurodesarrollo, se incorporan cuestionarios retrospectivos y actuales. La evaluación se fundamenta en los criterios del DSM-5 desde una perspectiva actualizada e integra información aportada por el entorno cercano (familia, pareja y amistades).',
   },
   {
     title: 'Evaluaciones de AACC (Altas Capacidades)',
     description:
-      'Detección del potencial cognitivo y socioemocional para orientar acompañamientos y entornos educativos acordes.',
+      'Detección del potencial cognitivo y socioemocional del/la consultante. Incluye evaluación retrospectiva y actual, evaluación neurocognitiva, de cognición social, de aspectos emocionales y pruebas de creatividad.',
   },
   {
     title: 'Evaluaciones de TOC (Trastorno Obsesivo-Copulsivo)',
     description:
-      'Análisis del impacto de las obsesiones y compulsiones para diseñar un plan terapéutico específico.',
+      'Análisis de la duración, intensidad y frecuencia de las obsesiones y compulsiones para diseñar un plan terapéutico específico.',
   },
   {
     title: 'Tratamientos TCC / ACT / EPR, desde un enfoque neuroafirmativo.',
@@ -33,5 +33,10 @@ export const servicesTexts = [
     title: 'Capacitaciones',
     description:
       'Formaciones teórico-prácticas para equipos e instituciones que desean integrar miradas neuroafirmativas en su trabajo.',
+  },
+  {
+    title: 'Evaluaciones integrales',
+    description:
+      'Se trata de nuestra evaluación central para el diagnóstico diferencial, ya que integra una valoración exhaustiva de múltiples condiciones, incluyendo Trastorno del Espectro Autista, TDAH, Altas Capacidades y rasgos de personalidad.',
   },
 ];
