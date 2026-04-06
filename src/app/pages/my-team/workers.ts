c4eed4f989c54ed8ef1882d0f33fa85a6271567a
Pablo Cocciaglia <cocciaglia@gmail.com>
2026-04-06 21:09:07 +0200

    change in team members

--- src/app/pages/my-team/workers.ts
+++ src/app/pages/my-team/workers.ts
@@ -2,41 +2,56 @@ export const coworkers = [
   {
     name: 'Lic. Camila Zajac',
     diploma: 'Lic. en Psicología',
+    description:
+      'Psicóloga recibida en la Universidad de Buenos Aires. Especializada en Autismo y TDAH. Marco teórico integrativo (TCC + ACT) Atención psicológica a población AuTDAH y AACC, adolescentes y adultos. Evaluaciones de TDAH.',
   },
-  {
-    name: 'Lic. Gisele Garro',
-    diploma: 'Lic. en Psicología',
-  },
+  // {
+  //   name: 'Lic. Gisele Garro',
+  //   diploma: 'Lic. en Psicología',
+  // },
   {
     name: 'Lic. Narella Di Nicolo',
     diploma: 'Lic. en Psicología',
+
+    description:
+      'Psicóloga recibida en la Universidad de Buenos Aires. Terapia integrativa (TCC + Mindfulness). Atención a población AuTDAH.',
   },
   {
-    name: 'Lic. Mónica Landa',
+    name: 'Lic. Mónica Landa Prieto',
     diploma: 'Lic. en Psicología',
+    description:
+      'Psicóloga recibida en la Universidad de Buenos Aires. Master en Atención Temprana en la Universidad de Barcelona. Formación en Terapias de Segunda y Tercera Ola. Atención a adolescentes y adultos (TDAH, Crisis Vitales, Depresión)',
   },
   {
     name: 'Lic. Andrea Achaval',
     diploma: 'Lic. en Psicología',
+    description:
+      'Psicóloga recibida en la Universidad Kennedy. Especializada en Trastorno de la Conducta Alimentaria y Dismorfia Corporal. Atención a adolescentes y adultos (TCA, Crisis vitales, Ansiedad, Depresión)',
   },
   {
     name: 'Lic. Micaela Aparicio',
     diploma: 'Lic. en Psicopedagogía',
+    description:
+      'Psicopedagoga recibida en la Universidad de Buenos Aires. Tratamiento a niños y adolescentes. Evaluación psicopedagógica y de TDAH.',
   },
   {
     name: 'Lic. Paula Palacios',
     diploma: 'Lic. en Psicopedagogía',
+    description:
+      'Psicopedagoga recibida en Universidad General San Martín. Tratamiento a niños, adolescentes y adultos.',
   },
   {
     name: 'Lic. Daniela Di Stéfano',
     diploma: 'Lic. En Terapia Ocupacional',
+    description:
+      'Terapista Ocupacional recibida en Universidad de Buenos Aires. Especializada en Evaluación y Tratamiento de Perfiles Sensoriales.',
   },
-  {
-    name: 'Lic. Laura López',
-    diploma: 'Lic. en Trabajo Social',
-  },
-  {
-    name: 'Laura Ruiz Díaz',
-    diploma: 'Traductora',
-  },
+  // {
+  //   name: 'Lic. Laura López',
+  //   diploma: 'Lic. en Trabajo Social',
+  // },
+  // {
+  //   name: 'Laura Ruiz Díaz',
+  //   diploma: 'Traductora',
+  // },
 ];
