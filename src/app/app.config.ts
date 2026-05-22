6d7815edcf5c525da8705513a61f9ccdf0bc046e
Pablo Cocciaglia <cocciaglia@gmail.com>
2026-05-22 05:45:24 +0200

    SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,12 +6,13 @@ import {
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch()), provideClientHydration(withEventReplay()),
   ],
 };
