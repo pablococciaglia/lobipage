3e11c99ff4dce934b802bd1519ee4da56712b46c
Pablo Cocciaglia <cocciaglia@gmail.com>
2026-06-11 18:26:26 +0200

    photos improvemnet and change to prod endpoint

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://mailer-back.cloud',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  baseURL: 'https://mailer-back.cloud/',
+  baseURL: 'https://mailer-back.cloud',
 };
